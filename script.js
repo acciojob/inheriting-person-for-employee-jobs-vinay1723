322308c392942b271be9c43eb3568c8083c14528
vinay1723 <144577819+vinay1723@users.noreply.github.com>
2025-07-04 22:28:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function Person(name, age) {
 }
 
 Person.prototype.greet = function() {
-	console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+	console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
 }
 function Employee(name, age, jobTitle) {
 	Person.call(this,name,age);
